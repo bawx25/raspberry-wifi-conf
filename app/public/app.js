9f99338e49488e5d72e4abee9d9dbe15aa368f2e
bawx25 <77675567+bawx25@users.noreply.github.com>
2021-01-25 15:51:56 +0100

    Update app.js

--- app/public/app.js
+++ app/public/app.js
@@ -9,10 +9,8 @@ var app = angular.module("RpiWifiConfig", []);
 /******************************************************************************\
 Function:
     AppController
-
 Dependencies:
     ...
-
 Description:
     Main application controller
 \******************************************************************************/
