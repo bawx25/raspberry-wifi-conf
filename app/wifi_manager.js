77d5dc8ea5f69168d81808bbd7057c56019b23ff
bawx25 <77675567+bawx25@users.noreply.github.com>
2021-01-27 10:10:47 +0100

    Update wifi_manager.js

--- app/wifi_manager.js
+++ app/wifi_manager.js
@@ -255,14 +255,14 @@ module.exports = function() {
             
 				
 				//Add new network
-		function update_wpa_supplicant(next_step) {
+				function update_wpa_supplicant(next_step) {
                     write_template_to_file(
                         "./assets/etc/wpa_supplicant/wpa_supplicant.conf.template",
                         "/etc/wpa_supplicant/wpa_template.template",
                         connection_info, next_step);
 				},
 		    
-		function update_wpa_supplicant(next_step) {
+				function update_wpa_supplicant(next_step) {
                     exec("cat /etc/wpa_supplicant/wpa_template.template >> /etc/wpa_supplicant/wpa_supplicant.conf", function(error, stdout, stderr) {
                         if (!error) console.log("... adding wifi network!");
                         else console.log("... adding wifi network failed! - " + stdout);
